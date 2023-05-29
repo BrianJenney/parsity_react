5abbf20c1b0fce69d8db4f56f852094461335fd5
Brian Jenney <brianjenney83@gmail.com>
2023-05-29 06:49:56 -0700

    update the constants file

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,12 @@
-// You can use this file to add all your constants
-
 export const CONSTANTS = {
     ADD_CONTACT: 'ADD CONTACT',
     EDIT_CONTACT: 'EDIT CONTACT',
     SAVE_CONTACT: 'SAVE CONTACT',
     SAVE_CHANGES: 'SAVE CHANGES',
     ADD_NEW_CONTACT: 'ADD NEW CONTACT',
+    FORM_IMAGE_URL: 'Image URL',
+    FORM_NAME: 'Name',
+    FORM_EMAIL: 'Email',
+    FORM_PHONE_NUMBER: 'Phone Number',
+    FORM_SUBMIT: 'Submit',
 };
